de0e36d390ef269052855868e3bc9255f9e4a724
thekim123 <tacdo12@naver.com>
2025-04-23 00:47:05 +0900

    fix: cors 설정 수정

--- src/main/java/com/boram/look/global/config/WebMvcConfig.java
+++ src/main/java/com/boram/look/global/config/WebMvcConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "OPTIONS", "DELETE")
-                .allowedOrigins("http://localhost:3000")
-                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000", "https://api.ondolook.click", "http://api.ondolook.click")
+                .allowedHeaders("X-DEVICE-ID", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+                .exposedHeaders(String.valueOf(Arrays.asList("Access-Control-Allow-Headers", "Authorization", "x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")))
                 .allowCredentials(true);
     }
 }
