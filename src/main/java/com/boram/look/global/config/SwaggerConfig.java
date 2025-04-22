8cf97424a9c60b8e080816cf18bb75864601ce64
thekim123 <tacdo12@naver.com>
2025-04-23 02:39:08 +0900

    feat: swagger 요청 url fix

--- src/main/java/com/boram/look/global/config/SwaggerConfig.java
+++ src/main/java/com/boram/look/global/config/SwaggerConfig.java
@@ -5,26 +5,38 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.profiles.active}")
+    private String active;
+
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("On The Look API 명세서"))
-
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .name("bearerAuth")
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                        )
-                );
+        OpenAPI openAPI = new OpenAPI();
+        if (Objects.equals(active, "prod")) {
+            openAPI.addServersItem(new Server().url("https://api.ondolook.click"));
+        } else {
+            openAPI.addServersItem(new Server().url("http://localhost:8080"));
+        }
+        return
+                openAPI.info(new Info().title("On The Look API 명세서"))
+                        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                        .components(new Components()
+                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                )
+                        );
     }
 
 }
