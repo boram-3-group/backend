9d54707dc4a5db281c7c24b6eb1f478d17cd503e
thekim123 <tacdo12@naver.com>
2025-05-13 22:13:42 +0900

    feat: fcmtoken - userid 관련 설정 추가

--- src/main/java/com/boram/look/domain/notification/FcmToken.java
+++ src/main/java/com/boram/look/domain/notification/FcmToken.java
@@ -2,6 +2,8 @@ package com.boram.look.domain.notification;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -16,6 +18,7 @@ public class FcmToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JdbcTypeCode(SqlTypes.BINARY)
     @Column(name = "user_id", columnDefinition = "BINARY(16)", nullable = false, updatable = false)
     private UUID userId;
     @Column(name = "fcm_token")
